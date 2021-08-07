82586ce124a4efb04b2394a26be9a87b8c644b5e
777marc@gmail.com <777marc@gmail.com>
2021-08-07 12:51:05 -0400

    test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_login()
+    {
+        $user = User::factory()->create();
+        $baseUrl = env('APP_URL') . '/api/auth/login';
+        $email = $user->email;
+        $password = "password";
+    
+        $response = $this->json('POST', $baseUrl . '/', [
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token', 'token_type', 'expires_in'
+        ]);
+    }
+
+    /** @test */
+    public function a_user_can_register()
+    {
+        $baseUrl = env('APP_URL') . '/api/auth/register';
+
+        $newUser = [
+            "name" => "marc",
+            "email" => "777marc@gmail.com",
+            "password" => "marc1111",
+            "password_confirmation" => "marc1111"
+        ];
+
+        $response = $this->json('POST', $baseUrl . '/', $newUser);
+        $response->assertStatus(201);
+        $this->assertEquals($newUser["name"], $response["user"]["name"]);
+    }
+
+}
